115c17944ddc58a21e05397e79395da5a4c875e1
martink-GOG <mkersten2004@gmail.com>
2026-09-16 11:04:25 +0200

    Initial Commit

---
+++ src/galgje/Galgje.java
@@ -0,0 +1,13 @@
+package galgje;
+
+public class Galgje {
+
+	public static void main(String[] args) {
+		String[] letterVanWoord= {"t", "o", "p"};
+		String[] gerradenLetters;
+		String[] geprobeerdenLetters;
+		int foutenPoging=11;
+
+	}
+
+}
